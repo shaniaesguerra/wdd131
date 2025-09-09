5a827ff34b757bcc27674810c76a96918f3010f7
shaniaesguerra <shaniae47@gmail.com>
2025-09-09 13:04:41 -0600

    adding more to .js for DOM Activity

--- week02/scripts/bom-app-jvs.js
+++ week02/scripts/bom-app-jvs.js
@@ -2,20 +2,41 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-//create an li element
-const li = document.createElement('li'); 
+//Focus on the input area
+input.focus();
 
-//create a button
-const deleteButton = document.createElement('button');
+//creating a click event listener for the ADD Chapter button
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        //create an li element
+        const li = document.createElement('li');
 
-//populate li element with input value
-li.textContent = input.value;
+        //create a button
+        const deleteButton = document.createElement('button');
 
-//populate button textContent with an X
-deleteButton.textContent = '❌';
+        //populate li element with input value
+        li.textContent = input.value;
 
-//append li element with button
-li.append(deleteButton);
+        //populate button textContent with an X
+        deleteButton.textContent = '❌';
 
-//append li element to the unordered list in the HTML 
-list.append(li);
+        //append li element with button
+        li.append(deleteButton);
+
+        //append li element to the unordered list in the HTML 
+        list.append(li);
+
+        //creating a click event listener for the DELETE button
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        //clean up input for the user
+        input.value = '';
+    }
+    else {
+        input.focus();
+    }
+});
+
