c629442b3780f72755e55783d0979064b7ce3504
shaniaesguerra <shaniae47@gmail.com>
2025-09-10 21:48:03 -0600

    adding hamburger menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,5 +3,18 @@ const modification = document.querySelector("#lastModified");
 const today = new Date();
 const modificationDate = document.lastModified;
 
+//get current year
 year.innerHTML = `<span id="currentyear">&copy ${today.getFullYear()}</span>`;
+
+//Get last modification date and time
 modification.innerHTML = `<p id="lastModified">Last Modified: ${modificationDate}</p>`;
+
+// Hamburger menu
+const burgerButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+//add click event for hamburger button
+burgerButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    burgerButton.classList.toggle("open");
+});
