162551b315c31e778d7817a64544bc7d01ff2482
shaniaesguerra <shaniae47@gmail.com>
2025-09-17 14:03:10 -0600

    set-up for js coding

--- scripts/place.js
+++ scripts/place.js
@@ -6,3 +6,5 @@ const modificationDate = document.lastModified;
 //Get last modified date and time ; get current year
 year.innerHTML = `<span id="currentyear">&copy ${today.getFullYear()}</span>`;
 modification.innerHTML = `<p id="lastModified">Last Modified: ${modificationDate}</p>`;
+
+//Calculate Wind Chill
