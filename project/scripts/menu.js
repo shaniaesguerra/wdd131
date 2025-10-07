1357a9598fea41688ef0ca7fb9a2b4f46aae8f3e
shaniaesguerra <shaniae47@gmail.com>
2025-10-07 11:23:08 -0600

    added menu array

---
+++ project/scripts/menu.js
@@ -0,0 +1,105 @@
+//menu items array
+const menu = [
+    {
+        id: 'bnbrd-001',
+        itemName: 'Plain Banana Bread',
+        ingredients: ['Bananas', 'Flour', 'Sugar', 'Eggs', 'Vegetable Oil',
+                      'Citrus Fruit Juice', 'Baking Powder', 'Baking Soda', 'Salt'],
+        quantity: 1,
+        price: 15.00
+    },
+    {
+        id: 'bnbrd-002',
+        itemName: 'Raisin and Walnuts Banana Bread',
+        ingredients: ['Bananas', 'Flour', 'Sugar', 'Eggs', 'Vegetable Oil',
+            'Citrus Fruit Juice', 'Raisins', 'Walnuts','Baking Powder', 'Baking Soda', 'Salt'],
+        quantity: 1,
+        price: 18.00
+    },
+    {
+        id: 'bnbrd-003',
+        itemName: 'Glazed Walnuts Banana Bread',
+        ingredients: ['Bananas', 'Flour', 'Sugar', 'Eggs', 'Vegetable Oil',
+            'Citrus Fruit Juice','Walnuts', 'Butter', 'Baking Powder', 'Baking Soda', 'Salt'],
+        quantity: 1,
+        price: 20.00
+    },
+    {
+        id: 'bnbrd-004',
+        itemName: 'Choco Chip Banana Bread',
+        ingredients: ['Bananas', 'Flour', 'Sugar', 'Eggs', 'Vegetable Oil',
+            'Citrus Fruit Juice', 'Choco Chips','Baking Powder', 'Baking Soda', 'Salt'],
+        quantity: 1,
+        price: 20.00
+    },
+    {
+        id: 'bnbrd-005',
+        itemName: 'Choco Chip and Walnuts Banana Bread',
+        ingredients: ['Bananas', 'Flour', 'Sugar', 'Eggs', 'Vegetable Oil',
+            'Citrus Fruit Juice', 'Choco Chips','Walnuts','Baking Powder', 'Baking Soda', 'Salt'],
+        quantity: 1,
+        price: 20.00
+    },
+    {
+        id: 'bnbrd-006',
+        itemName: ' Blueberry with Streusel Topping Banana Bread',
+        ingredients: ['Bananas', 'Flour', 'Sugar', 'Eggs', 'Vegetable Oil',
+            'Citrus Fruit Juice', 'Blueberries', 'Walnuts', 'Butter','Baking Powder', 'Baking Soda', 'Salt'],
+        quantity: 1,
+        price: 22.00
+    },
+    {
+        id: 'fbrd-001',
+        itemName: 'Spanish Bread',
+        ingredients: ['Bread Flour', 'Milk', 'Sugar', 'Margarine',
+            'Eggs', 'Breadcrumbs', 'Condensed Milk', 'Salt', 'Yeast'],
+        quantity: 5,
+        price: 7.00
+    },
+    {
+        id: 'fbrd-002',
+        itemName: 'Pandesal',
+        ingredients: ['Flour', 'Milk', 'Sugar', 'Margarine',
+            'Eggs', 'Breadcrumbs', 'Salt', 'Yeast'],
+        quantity: 12,
+        price: 7.00
+    },
+    {
+        id: 'fbrd-003',
+        itemName: 'Classic Ensaymada (6pcs)',
+        ingredients: ['Flour', 'Milk', 'Sugar', 'Margarine',
+            'Eggs', 'Breadcrumbs', 'Salt', 'Yeast'],
+        quantity: 6,
+        price: 15.00
+    },
+    {
+        id: 'fbrd-004',
+        itemName: 'Classic Ensaymada (1pc)',
+        ingredients: ['Flour', 'Milk', 'Sugar', 'Margarine',
+            'Eggs', 'Breadcrumbs', 'Salt', 'Yeast'],
+        quantity: 1,
+        price: 4.00
+    },
+    {
+        id: 'fbrd-005',
+        itemName: 'Cream Cheese Ensaymada (6pcs)',
+        ingredients: ['Flour', 'Milk', 'Sugar', 'Margarine',
+            'Eggs', 'Breadcrumbs', 'Cream Cheese', 'Cheese', 'Butter', 'Icing Sugar', 'Salt', 'Yeast'],
+        quantity: 6,
+        price: 25.00
+    },
+    {
+        id: 'fbrd-006',
+        itemName: 'Cream Cheese Ensaymada (1pc)',
+        ingredients: ['Flour', 'Milk', 'Sugar', 'Margarine',
+            'Eggs', 'Breadcrumbs', 'Cream Cheese', 'Cheese','Butter', 'Icing Sugar', 'Salt', 'Yeast'],
+        quantity: 1,
+        price: 6.00
+    },
+
+];
+
+//store number of times order button is clicked in local Storage
+
+
+//populate grid with menu cards
